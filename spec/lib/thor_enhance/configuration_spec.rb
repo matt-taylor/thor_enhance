78478b6799f22ea45b4047543ee9ab8af27dd15b
Matt Taylor <mattius.taylor@gmail.com>
2023-11-21 15:30:08 -0800

    update tests

--- spec/lib/thor_enhance/configuration_spec.rb
+++ spec/lib/thor_enhance/configuration_spec.rb
@@ -4,16 +4,33 @@ RSpec.describe ThorEnhance::Configuration do
   let(:instance) { described_class.new }
 
   before { described_class.class_variable_set("@@allow_changes", nil) }
+  let(:random_chars) { (("A".."Z").to_a +  ("a"..."z").to_a).shuffle[0..20].join }
 
   describe "#add_command_method_enhance" do
     subject { instance.add_command_method_enhance(name, allowed_klasses: allowed_klasses, enums: enums) }
 
     let(:allowed_klasses) { nil }
     let(:enums) { nil }
-    let(:name) { "some_name_#{rand}" }
+    let(:name) { "some_name_#{random_chars}" }
+
+    context "when invalid type" do
+      let(:name) { 1 }
+
+      it do
+        expect { subject }.to raise_error(ArgumentError, /Invalid name type received/)
+      end
+    end
+
+    context "when invalid name" do
+      let(:name) { ".wfwa3145" }
+
+      it do
+        expect { subject }.to raise_error(ArgumentError, /Invalid name received/)
+      end
+    end
 
     context "when already defined" do
-      let(:name) { ::Thor::Command.instance_methods.sample }
+      let(:name) { ::Thor::Command.instance_methods.select { _1 =~ /^[A-Za-z_]+$/ }.sample }
 
       it do
         expect { subject }.to raise_error(ThorEnhance::OptionNotAllowed, /is not allowed as an enhancement/)
@@ -40,10 +57,26 @@ RSpec.describe ThorEnhance::Configuration do
 
     let(:allowed_klasses) { nil }
     let(:enums) { nil }
-    let(:name) { "some_name_#{rand}" }
+    let(:name) { "some_name_#{random_chars}" }
+
+    context "when invalid type" do
+      let(:name) { 1 }
+
+      it do
+        expect { subject }.to raise_error(ArgumentError, /Invalid name type received/)
+      end
+    end
+
+    context "when invalid name" do
+      let(:name) { ".wfwa3145" }
+
+      it do
+        expect { subject }.to raise_error(ArgumentError, /Invalid name received/)
+      end
+    end
 
     context "when already defined" do
-      let(:name) { ::Thor::Option.instance_methods.sample }
+      let(:name) { ::Thor::Option.instance_methods.select { _1 =~ /^[A-Za-z_]+$/ }.sample }
 
       it do
         expect { subject }.to raise_error(ThorEnhance::OptionNotAllowed, /is not allowed as an enhancement/)
